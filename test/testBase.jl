b171964fd9d317784bb2259d76e751be6daab2c2
Lewis Hein <lhein@vcn.com>
2016-03-18 10:39:36 -0600

    Added test for the array version of insert!

--- test/testBase.jl
+++ test/testBase.jl
@@ -5,7 +5,7 @@ function randfloat()
 	return 2*(1-rand(Float64))::Float64
 end
 
-max_len = 10000.0
+max_len = 100.0
 len = rand(1:max_len)
 knots = sort(unique([randfloat()*len::Float64 for i in 1:len]))
 len = length(knots)
@@ -30,8 +30,8 @@ s = Spline(knots, values)
 #Test for refusal to work with NaN values
 knotsNaN = copy(knots)
 valuesNAN = copy(values)
-knotsNaN[Int(floor(rand()*len))] = NaN
-valuesNAN[Int(floor(rand()*len))] = NaN
+knotsNaN[Int(floor(rand()*len))] = NaN #FIXME: this could generate errors with accessing element 0
+valuesNAN[Int(floor(rand()*len))] = NaN #FIXME: this could generate errors with accessing element 0
 @test_throws ArgumentError Spline(knotsNaN, values)
 @test_throws ArgumentError Spline(knots, valuesNAN)
 @test_throws ArgumentError Spline(knotsNaN, valuesNAN)
@@ -49,7 +49,7 @@ knotsDup[dupInd] = knotsDup[dupInd2]
 
 domainMin = minimum(knots)
 domainMax = maximum(knots)
-@test domain(s) == domainMin:domainMax
+@test domain(s) == (domainMin, domainMax)
 
 value_indomain = rand(domainMin:domainMax)
 value_notindomain = value_indomain + domainMax + rand(Float64)
@@ -63,8 +63,8 @@ end
 
 function test_values()
 	tol = rand(Float64)/10
-	disc = discretize(s, tol)
-	mesh = Splines.discretize_mesh(s, tol)
+	disc = uniform_discretize(s, tol)
+	mesh = Splines.uniform_discretize_mesh(s, tol)
 	for i in eachindex(disc)
 		@test s(mesh[i]) ==  disc[i]
 	end
@@ -85,5 +85,19 @@ test_values()
 
 ##### insert!
 # just change value if knot is already there
-insert!(s, Float64(randindex()),  rand()) 
+insert!(s, Float64(randindex()),  rand())
 test_values()
+#inserting a value actually adds a knot
+newknot = Float64(randindex()+randfloat())
+newvalue = rand()
+insert!(s, newknot, newvalue)
+@test s(newknot) == newvalue
+
+#the array version of insert also works
+newknots = [Float64(rand(domain(s)[1]:rand():domain(s)[2])) for i in 1:rand(1:10)]
+newvalues = [rand() for i in newknots]
+insert!(s, newknots, newvalues)
+for (i, knot) in enumerate(newknots)
+	@test s(knot) == newvalues[i]
+end
+##### End insert! tests
